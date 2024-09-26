cf33de1d720bd76797e8169e7903d1569441a972
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-09-26 10:09:45 +0530

    updates on side bar

--- lms/app/(dashboard)/_components/sidebar-item.tsx
+++ lms/app/(dashboard)/_components/sidebar-item.tsx
@@ -1,6 +1,10 @@
 "use client ";
 
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
 
 interface SideBarItemProps {
   icon: LucideIcon;
@@ -8,6 +12,34 @@ interface SideBarItemProps {
   href: string;
 }
 
-export const SidebarItem = ({ icon, label, href }: SideBarItemProps) => {
-  return <div>SidebarItem!qq</div>;
+export const SidebarItem = ({ icon: Icon, label, href }: SideBarItemProps) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const isActive =
+    (pathname === "/ " && href === "/ ") ||
+    pathname === href ||
+    pathname?.startsWith(`${href}`);
+
+  const onClick = () => {
+    router.push(href);
+  };
+
+  return (
+    <Button
+      onClick={onClick}
+      type="button"
+      className={cn(
+        "flex items  -center gap-x-2 text-slate-500  text-sm font[500] pl-6 transition-all hover:text-slate-600 hover: bg-slate-300/20",
+        isActive &&
+          "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+      )}
+    >
+      <div className="flex items-center gap-x-2 py-4">
+        <Icon
+          size={22}
+          className={cn("text-slate-500", isActive && "text-sky-700")}
+        />
+      </div>
+    </Button>
+  );
 };
