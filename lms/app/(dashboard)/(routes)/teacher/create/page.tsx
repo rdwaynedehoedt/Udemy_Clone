708052134a048b96439b9ce02a9200fec9dd7e22
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-10-03 15:03:11 +0530

    so far so good i did something tht i dont know lol

--- lms/app/(dashboard)/(routes)/teacher/create/page.tsx
+++ lms/app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -3,11 +3,88 @@
 import * as z from "zod";
 import axios from "axios";
 import {zodResolver} from "@hookform/resolvers/zod";
- 
+import {useForm} from "react-hook-form";
+import { useRouter} from "next/router";
+import Link from "next/link";
+
+import {    
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage 
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+  
+const formSchema = z.object({
+    title: z.string ().min(1,{message:"Title is required"}),
+})
+
 const CreatePage = () => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues:{
+            title:""
+        },
+    })
+
+    const {isSubmitting,  isValid } = form. formState;
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
+
+
     return ( 
-        <div>
-            <p>Create Page</p>
+        <div className="max-w-5xl mx-auto flex md:items-center  md:justify-centerh h-full p-6">
+            <div className="text-2xl">
+                 <h1>
+                     Name your course
+                 </h1>
+                 <p className="text-sm text-slate-600">
+                    What would you like to name your course? Don't worry you can change this latter.
+                 </p>
+                 <Form {...form}>
+                     <form onSubmit={form.handleSubmit(onSubmit )} className="space-y-8 mt-8">
+                          <FormField 
+                            control={form.control}
+                            name="title"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Course Title  
+                                    </FormLabel>
+                                    <FormControl>
+                                            <Input 
+                                                disabled={isSubmitting}
+                                                placeholder="e.g.'Advance web development'"
+                                                {...field}
+                                             />
+                                        </FormControl>
+                                    <FormDescription>
+                                            What you will teach in this course?
+                                        </FormDescription>
+                                        <FormMessage />
+                                </FormItem>
+                            )}
+                          />
+                          <div className="flex item-center gap-x-2">
+                            <Link href="/teacher/courses">
+                            <Button type="button"  variant="ghost">
+                                Cancel
+                            </Button>
+                            </Link>
+                            <Button type="submit">
+                                Continue
+                            </Button>
+                          </div>
+                     </form>
+                 </Form>
+            </div>
         </div>
      );
 }
--- lms/components/ui/button.tsx
+++ lms/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: " hover:bg-[# ] transition-colors duration-200 ease-in-out",
+        ghost: "text-black hover:bg-[#007DFC] hover:text-white transition-colors duration-200 ease-in-out",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
