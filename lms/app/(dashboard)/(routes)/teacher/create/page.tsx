b01a077be0c7a5a21838e822ab4622f2b17ec82f
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-10-03 10:34:33 +0530

    so far so good i did something tht i dont know lol

---
+++ lms/app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import {zodResolver} from "@hookform/resolvers/zod";
+ 
+const CreatePage = () => {
+    return ( 
+        <div>
+            <p>Create Page</p>
+        </div>
+     );
+}
+ 
+export default CreatePage;
