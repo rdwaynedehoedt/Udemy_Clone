2ab716668dbd664f33072d126a3ee7948bfec0bf
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-10-03 21:55:48 +0530

    new updates on the form

--- lms/app/(dashboard)/(routes)/teacher/create/page.tsx
+++ lms/app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -4,7 +4,7 @@ import * as z from "zod";
 import axios from "axios";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
-import { useRouter} from "next/router";
+import { useRouter} from "next/navigation";
 import Link from "next/link";
 
 import {    
@@ -24,7 +24,7 @@ const formSchema = z.object({
 })
 
 const CreatePage = () => {
-
+    const router = useRouter();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues:{
@@ -34,13 +34,18 @@ const CreatePage = () => {
 
     const {isSubmitting,  isValid } = form. formState;
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try{
+            const response = await axios.post("/api/course" , values)
+            router.push('/teacher/courses/${respose.data.id}');
+        } catch {
+            console.log("Something went wrong");
+        }
     }
 
 
     return ( 
-        <div className="max-w-5xl mx-auto flex md:items-center  md:justify-centerh h-full p-6">
+        <div className="max-w-5xl mx-auto flex md:items-center items-center justify-center md:justify-centerh h-full p-6">
             <div className="text-2xl">
                  <h1>
                      Name your course
@@ -78,7 +83,7 @@ const CreatePage = () => {
                                 Cancel
                             </Button>
                             </Link>
-                            <Button type="submit">
+                            <Button type="submit" disabled={!isValid || isSubmitting}>
                                 Continue
                             </Button>
                           </div>
